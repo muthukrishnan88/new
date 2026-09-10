f10ea9897fea52c861a9ea0e518830964a9bc1a7
siva0990 <sivasabarip0990official@gmail.com>
2026-09-10 07:53:02 +0530

    Serve SAFNEX NOVA frontend

--- link_server.js
+++ link_server.js
@@ -3224,6 +3224,17 @@ app.use(
 /* =========================================================
    START
 ========================================================= */
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(
     PORT,
